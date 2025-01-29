bf737ffe93a55b0ba2528443f9c52d92b4aaa96a
Radu Gheorghe <radu@vespa.ai>
2025-01-29 14:41:01 +0200

    demo files for Big Data Meetup Bucharest

---
+++ bigdata_2025-02/demo.sh
@@ -0,0 +1,11 @@
+# Vespa container
+
+podman run --name vespa-bigdatabu --hostname vespa-bigdatabu \
+  --publish 8080:8080 --publish 19071:19071 \
+  vespaengine/vespa
+
+# tensor playground
+# https://docs.vespa.ai/playground/#N4KABGBEBmkFxgNrgmUrWQPYAd5QFNIAaFDSPBdDTAO30gGMsAbAExxYEMBPEsmpCIJIAFwK0AzlgBOACgDmEmQWABfAJRxgMrIwDWcAAwA6AIzEcuYyYDsayAIhqBa0hmrlcDIu8GU0JzoGAFsCUS4WFgBLRi5+GkxhKHEpWUVlVU1tXQMbAE5La1MAJgcglww3AU9MbxFfIIp8WvJ6ERCeCMlxBMTCBlTpeSVaFXUtHT1DU1sinBsAZnLEytRqjyb6gb9yANbgkWZ2Tl4AWhCuUUYACz7EoQZJAFcQuWOObh4wACowTu64mIYFGKg0jlWrl2gQe20e0IgzSoQUR7SgH1OPAuV1uZzY4S40RYBDY90EySYrE+vF+-y6XB6RAqUJqWwC8Ka+xRUDRkDCESisS4F3pjLJ5ApLze-MiMTitIBDKBIMy4OZVWhB2w7MaDy5-R5oQJgri2OuNzxBKJJPFSSNArlXAVot66vWKAAuiA1EA
+
+# ColPali demo
+# https://huggingface.co/spaces/vespa-engine/colpali-vespa-visual-retrieval
