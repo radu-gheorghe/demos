1407ce57284a0de280b92dec069a1e91f47439a0
Radu Gheorghe <radu@vespa.ai>
2025-02-26 14:38:25 +0200

    polishing big data demo

--- bigdata_2025-02/demo.sh
+++ bigdata_2025-02/demo.sh
@@ -4,8 +4,16 @@ podman run --name vespa-bigdatabu --hostname vespa-bigdatabu \
   --publish 8080:8080 --publish 19071:19071 \
   vespaengine/vespa
 
-# tensor playground
-# https://docs.vespa.ai/playground/#N4KABGBEBmkFxgNrgmUrWQPYAd5QFNIAaFDSPBdDTAO30gGMsAbAExxYEMBPEsmpCIJIAFwK0AzlgBOACgDmEmQWABfAJRxgMrIwDWcAAwA6AIzEcuYyYDsayAIhqBa0hmrlcDIu8GU0JzoGAFsCUS4WFgBLRi5+GkxhKHEpWUVlVU1tXQMbAE5La1MAJgcglww3AU9MbxFfIIp8WvJ6ERCeCMlxBMTCBlTpeSVaFXUtHT1DU1sinBsAZnLEytRqjyb6gb9yANbgkWZ2Tl4AWhCuUUYACz7EoQZJAFcQuWOObh4wACowTu64mIYFGKg0jlWrl2gQe20e0IgzSoQUR7SgH1OPAuV1uZzY4S40RYBDY90EySYrE+vF+-y6XB6RAqUJqWwC8Ka+xRUDRkDCESisS4F3pjLJ5ApLze-MiMTitIBDKBIMy4OZVWhB2w7MaDy5-R5oQJgri2OuNzxBKJJPFSSNArlXAVot66vWKAAuiA1EA
+# lexical search application package
 
-# ColPali demo
-# https://huggingface.co/spaces/vespa-engine/colpali-vespa-visual-retrieval
+vespa deploy ecommerce
+
+# semantic search application package
+
+vespa deploy embedder_ann
+
+# feed sample data for semantic search
+# NOTE: you can also use Logstash to feed data into Vespa
+#       see https://blog.vespa.ai/logstash-vespa-tutorials/
+
+vespa feed vespa_feed-1K_no_embeddings.jsonl
--- introduction_to_vespa/rag/queries.sh
+++ introduction_to_vespa/rag/queries.sh
@@ -1,6 +1,6 @@
 # traceLevel=1 will show the prompt and some stats
 vespa query \
-    --header="X-LLM-API-KEY:YOUR_OPENAI_KEY_GOES_HERE" \
+    --header="X-LLM-API-KEY:OPENAI_API_KEY_GOES_HERE" \
     query="what kind of mini stencils do you have for sale?" \
     query_text="Mini Stencil" \
     yql="select * from product where title contains @query_text OR ({targetHits:100}nearestNeighbor(titledesc_embedding,query_embedding))" \
